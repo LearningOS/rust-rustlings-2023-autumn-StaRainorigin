ab75466e6cd4919d4d539a623d5efa751d9b4231
StaRainorigin <1343026891@qq.com>
2023-10-16 22:09:01 +0800

    65

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -4,30 +4,32 @@
 // - Generics
 // - Traits
 //
-// An imaginary magical school has a new report card generation system written
-// in Rust! Currently the system only supports creating report cards where the
-// student's grade is represented numerically (e.g. 1.0 -> 5.5). However, the
-// school also issues alphabetical grades (A+ -> F-) and needs to be able to
-// print both types of report card!
+// An imaginary magical school has a new report card generation system written in Rust! 
+// Currently the system only supports creating report cards where the student's grade is represented numerically (e.g. 1.0 -> 5.5).
+// However, the school also issues alphabetical grades (A+ -> F-) and needs to be able to print both types of report card!
+// 
+// 
 //
-// Make the necessary code changes in the struct ReportCard and the impl block
-// to support alphabetical report cards. Change the Grade in the second test to
-// "A+" to show that your changes allow alphabetical grades.
+// Make the necessary code changes in the struct ReportCard and the impl block to support alphabetical report cards. 
+// Change the Grade in the second test to "A+" to show that your changes allow alphabetical grades.
 //
+// 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: f32, 
     pub student_name: String,
     pub student_age: u8,
+
 }
 
 impl ReportCard {
-    pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+    pub fn print(&self, alphabetical: bool) -> String {
+        if alphabetical {
+            format!("{} ({}) - achieved a grade of {}",&self.student_name, &self.student_age, "A+")
+        } else {
+            format!("{} ({}) - achieved a grade of {}",&self.student_name, &self.student_age, &self.grade)
+        }
     }
 }
 
@@ -43,21 +45,21 @@ mod tests {
             student_age: 12,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print(false),
             "Tom Wriggle (12) - achieved a grade of 2.1"
         );
     }
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
+
         let report_card = ReportCard {
             grade: 2.1,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print(true),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
@@ -29,8 +28,8 @@ impl OtherTrait for SomeStruct {}
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
-// YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+
+fn some_func<T: SomeTrait + OtherTrait>(item: T) -> bool {
     item.some_function() && item.other_function()
 }
 
@@ -38,3 +37,5 @@ fn main() {
     some_func(SomeStruct {});
     some_func(OtherStruct {});
 }
+
+// YOU MAY ONLY CHANGE THE NEXT LINE
